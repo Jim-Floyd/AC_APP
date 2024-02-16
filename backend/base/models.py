77c35964535b42dc57ef82fafc94a01d50835a9d
Jim-Floyd <jamshid.elboev5111995@gmail.com>
2024-02-16 12:19:32 +0500

    models are almost done

--- backend/base/models.py
+++ backend/base/models.py
@@ -15,12 +15,6 @@ class User(AbstractUser):
     )
 
 
-class Ac_warehouse(models.Model):
-    name = models.CharField()
-    warehouse_holder = models.ForeignKey(User, on_delete=models.CASCADE)
-    model_amount = models.IntegerField()
-
-
 class Ac_model_brand(models.Model):
     name = models.CharField(max_length = 20)
 
@@ -33,9 +27,16 @@ class Ac_model(models.Model):
     model_type = models.Foreign_Key(Ac_model_type, on_delete=models.CASCADE)
     model_price = models.IntegerField()
     model_margin = models.IntegerField()
+    model_cash_price = models.IntegerField()
     model_dealer_price = models.IntegerField()
-    model_warehouse = models.ManyToManyField(Ac_warehouse, related_name="models")
+    price_since = models.DateTimeField()
 
+
+class Ac_warehouse(models.Model):
+    name = models.CharField()
+    warehouse_holder = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="owned_warehouse")
+    model_amount = models.IntegerField()
+    model = models.ManyToManyField(Ac_model, related_name="warehouse")
 
 class Ac_payment_type(models.Model):
     name = models.CharField()
@@ -57,14 +58,25 @@ class Ac_app(models.Model):
     payment_type = models.ForeignKey(Ac_payment_type, on_delete=models.CASCADE, related_name="apps")
     payment_status = models.ForeignKey(Ac_payment_status, on_delete=models.CASCADE, related_name="apps")
     warehouse = models.ForeignKey(Ac_warehouse, on_delete=models.CASCADE, related_name="apps")
+    note = models.CharField(max_length = 70)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_now=True)
     is_canceled = models.BooleanField(default=False)
     canceled_at = models.DateTimeField()
-    is_shipped = models.BooleanField(default= False)
+    is_shipped = models.BooleanField(default=False)
     shipped_at = models.DateTimeField()
-    is_paid = models.BooleanField(default = False)
+    returned_amount = models.IntegerField()
+    returned_at = models.DateTimeField()
+    paid_summ = models.IntegerField()
     paid_at = models.DateTimeField()
+    paid_by_contr_summ = models.IntegerField()
+    paid_by_contr_at = models.DateTimeField()
+    contr_num = models.CharField(max_length = 70)
+    returned_payment = models.IntegerField()
+    returned_payment_at = models.DateTimeField()
+    returned_payment_why = models.CharField(max_length = 70)
+    manager_debt = models.IntegerField()
+    
 
 
 
